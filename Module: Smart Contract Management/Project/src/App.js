bbd901c3fca63cce575aa7490477bca741420a59
Aaditya Chandankar <aadityachandankar@gmail.com>
2023-07-13 15:26:00 +0530

    Updated Module 2 Smart Contract Management Project Files

---
+++ Module: Smart Contract Management/Project/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SimpleStorage from "./SimpleStorage";
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleStorage />
+    </div>
+  );
+}
+
+export default App;
